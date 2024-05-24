b1a0ee982b56ece595db9e24ab484f3fa8d46abe
Patrik Holler <82374078+hollepat@users.noreply.github.com>
2024-05-24 11:03:29 +0200

    Update Logger.py

--- Logger/Logger.py
+++ Logger/Logger.py
@@ -1,73 +1,27 @@
 import json
 import logging.config
 import logging.handlers
-import pathlib
+from pathlib import Path
 import logging
-import datetime
-
-LOG_FORMAT = '%(asctime)s | %(levelname)8s | %(filename)s:%(lineno)d | %(message)s'
 
 
-class CustomLogFormatter(logging.Formatter):
-    black = '\x1b[30m'
-    white = '\x1b[37m'
-    magenta = '\x1b[35m',
-    cyan = '\x1b[36m',
-    green = '\x1b[92m'
-    blue = '\x1b[38;5;39m'
-    yellow = '\x1b[38;5;226m'
-    red = '\x1b[38;5;196m'
-    bold_red = '\x1b[31;1m'
-    background_red = '\x1b[41m'
-    reset = '\x1b[0m'
+def setup_logging(log_file: Path) -> None:
+    """Set up logging.
 
-    def __init__(self, fmt):
-        self.fmt = fmt
-        super().__init__()
-        self.FORMATS = {
-            logging.DEBUG: self.blue + self.fmt + self.reset,
-            logging.INFO: self.green + self.fmt + self.reset,
-            logging.WARNING: self.yellow + self.fmt + self.reset,
-            logging.ERROR: self.red + self.fmt + self.reset,
-            logging.CRITICAL: self.black + self.background_red + self.fmt + self.reset
-        }
-
-    def format(self, record):
-        log_fmt = self.FORMATS.get(record.levelno)
-        formatter = logging.Formatter(log_fmt)
-        return formatter.format(record)
-
-
-def setup_logger(name, log_to_file=False, level=logging.DEBUG):
-    logger = logging.getLogger(name)
-
-    stdout_handler = logging.StreamHandler()
-    stdout_handler.setFormatter(CustomLogFormatter(LOG_FORMAT))
-    logger.addHandler(stdout_handler)
-
-    # remove standard logger
-    logger.propagate = False
-
-    if log_to_file:
-        today = datetime.datetime.now()
-        file_handler = logging.FileHandler('run_{}.log'.format(today.strftime('%Y_%m_%d-%H_%M')))
-        file_handler.setFormatter(logging.Formatter(LOG_FORMAT))
-        logger.addHandler(file_handler)
-
-    logger.setLevel(level)
-    return logger
-
-
-
-def setup_logging(log_file: str) -> None:
-    config_file = pathlib.Path("Logger/config.json")
+    :param log_file: Absolute path to log file
+    """
+    config_file = Path("Logger/config.json")
     if config_file.exists():
         with open(config_file, "rt") as file:
             config = json.load(file)
         logging.config.dictConfig(config)
         if "handlers" in config and "file" in config["handlers"]:
+            if not log_file.exists():
+                log_file.parent.mkdir(parents=True, exist_ok=True)
+                log_file.touch(exist_ok=True)
             config["handlers"]["file"]["filename"] = log_file
             logging.config.dictConfig(config)
+            print(f"Syncing to {log_file}")
     else:
         print("Config file not found. Using basic configuration.")
         logging.basicConfig(level=logging.INFO)
