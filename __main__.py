a5b791d429419988adf868c814bf507916021461
Patrik Holler <82374078+hollepat@users.noreply.github.com>
2024-05-24 11:03:47 +0200

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -2,24 +2,25 @@ import argparse
 import logging
 import Logger.Logger as Logger
 from synchronizer.FolderSynchronization import FolderSynchronization
+from pathlib import Path
 
 
 def parse_args():
     parser = argparse.ArgumentParser(description='File Synchronization')
 
     # positional arguments
-    parser.add_argument(dest='source', type=str, help='source directory')
-    parser.add_argument(dest='replica', type=str, help='replica directory')
+    parser.add_argument(dest='source', type=str, help='Absolute path to source directory.')
+    parser.add_argument(dest='replica', type=str, help='Absolute path replica directory.')
 
     # optional arguments
-    parser.add_argument('-i', dest='interval', type=int, help='synchronization interval in seconds')
-    parser.add_argument('-lf', dest='log_file', type=str, help='log file path', default='logs/sync.log')
+    parser.add_argument('-i', dest='interval', type=int, help='Synchronization interval in seconds.', default=2)
+    parser.add_argument('-lf', dest='log_file', type=str, help='Absolute path to log file.', default='logs/sync.log')
     return parser.parse_args()
 
 
 def main():
     args = parse_args()
-    Logger.setup_logging(args.log_file)
+    Logger.setup_logging(Path(args.log_file))
     fs = FolderSynchronization(args.source, args.replica, args.interval)
     try:
         fs.run()
