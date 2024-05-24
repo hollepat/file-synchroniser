913e48c996f980125034a53747c2e85974fad725
Patrik Holler <82374078+hollepat@users.noreply.github.com>
2024-05-24 11:03:04 +0200

    Update FolderHandler.py

--- synchronizer/FolderHandler.py
+++ synchronizer/FolderHandler.py
@@ -1,8 +1,8 @@
 from pathlib import Path
 import logging
-from typing import List
 import shutil
 import os
+import hashlib
 
 
 class FolderHandler:
@@ -11,47 +11,71 @@ class FolderHandler:
         self.source = source
         self.replica = replica
 
-    def update(self, differences: List[Path]):
-        """Update the replica folder based on the differences found."""
-        for item in differences:
-            if item.is_file():
-                self._update_file(item)
-            else:
-                self._update_folder(item)
+    def update(self):
+        for root, dirs, files in os.walk(self.source):
+
+            for filename in files:
+                try:
+                    replica_file_version = self.replica / Path(root).relative_to(self.source) / filename
+                    source_file_version = Path(root, filename)
+                    if not replica_file_version.exists():
+                        # file not exists in replica
+                        shutil.copy2(str(source_file_version), str(replica_file_version))
+                        logging.info(f"Created file {replica_file_version}")
+                    elif replica_file_version.exists() and self.calculate_checksum(
+                            replica_file_version) != self.calculate_checksum(source_file_version):
+                        # file exists but has changed
+                        shutil.copy2(str(source_file_version), str(replica_file_version))
+                        logging.info(f"Copying file {source_file_version} to {replica_file_version}")
+                    else:
+                        pass
+                except Exception as e:
+                    logging.error(f"An error occurred while updating file {Path(root, filename)}: {e}")
+
+            for directory in dirs:
+                try:
+                    replica_folder_version = self.replica / Path(root).relative_to(self.source) / directory
+                    if not replica_folder_version.exists():
+                        replica_folder_version.mkdir()
+                        logging.info(f"Created folder {replica_folder_version}")
+                except Exception as e:
+                    logging.error(f"An error occurred while updating folder {Path(root, directory)}: {e}")
 
     def delete_files_folders_replica(self):
         """Delete the file in the replica folder."""
         for root, dirs, files in os.walk(self.replica):
-
             for filename in files:
-                file_source = self.source / Path(root).relative_to(self.replica) / filename
-                if not file_source.exists():
-                    os.remove(Path(root, filename))
-                    logging.info(f"Deleted file {Path(root, filename)}")
+                try:
+                    file_source = self.source / Path(root).relative_to(self.replica) / filename
+                    if not file_source.exists():
+                        os.remove(Path(root, filename))
+                        logging.info(f"Deleted file {Path(root, filename)}")
+                except Exception as e:
+                    logging.error(f"An error occurred while deleting {Path(root, filename)}: {e}")
 
             for directory in dirs:
-                directory_source = self.source / Path(root).relative_to(self.replica) / directory
-                if not directory_source.exists():
-                    shutil.rmtree(Path(root, directory))
-                    logging.info(f"Deleted folder {Path(root, directory)}")
-    def _update_file(self, file_source: Path):
-        """Update the file in the replica folder."""
-        file_replica = self.replica / file_source.relative_to(self.source)
-        os.makedirs(file_replica.parent, exist_ok=True)
-        if not file_replica.exists():
-            shutil.copy2(str(file_source), str(file_replica))
-            logging.info(f"Created file {file_replica}")
-        else:
-            shutil.copy2(str(file_source), str(file_replica))
-            logging.info(f"Copying file {file_source} to {file_replica}")
+                try:
+                    directory_source = self.source / Path(root).relative_to(self.replica) / directory
+                    if not directory_source.exists():
+                        shutil.rmtree(Path(root, directory))
+                        logging.info(f"Deleted folder {Path(root, directory)}")
+                except Exception as e:
+                    logging.error(f"An error occurred while deleting {Path(root, directory)}: {e}")
 
-    def _update_folder(self, folder_source: Path):
-        """Update the folder in the replica folder."""
-        folder_replica = self.replica / folder_source.relative_to(self.source)
-        if not folder_replica.exists():
-            shutil.copytree(str(folder_source), str(folder_replica))
-            logging.info(f"Created folder {folder_replica}")
-        else:
-            shutil.rmtree(str(folder_replica))
-            shutil.copytree(str(folder_source), str(folder_replica))
-            logging.info(f"Copying folder {folder_source} to {folder_replica}")
+    @staticmethod
+    def calculate_checksum(file_path: Path, algorithm='md5') -> str:
+        hash_func = hashlib.new(algorithm)
+        try:
+            with open(file_path, 'rb') as f:
+                while chunk := f.read(4096):
+                    hash_func.update(chunk)
+        except FileNotFoundError as e:
+            logging.error(f"File not found: {file_path} - {e}")
+            raise
+        except PermissionError as e:
+            logging.error(f"Permission denied: {file_path} - {e}")
+            raise
+        except OSError as e:
+            logging.error(f"OS error for file {file_path}: {e}")
+            raise
+        return hash_func.hexdigest()
